51a8d3decabb19ede8bfe9343085543cfc6280e8
Jeremias Menichelli <jmenichelli@gmail.com>
2018-10-26 14:03:08 +0200

    chore: new mocked payload added

---
+++ test/mocked-payload.js
@@ -0,0 +1,348 @@
+export default {
+   "form_id":"CEIcOU",
+   "token":"0c73d104418c428d958240e12734f72c",
+   "landed_at":"2018-10-26T10:33:57Z",
+   "submitted_at":"2018-10-26T10:36:06Z",
+   "definition":{
+      "id":"CEIcOU",
+      "title":"Pizzas",
+      "fields":[
+         {
+            "id":"NGXQV4HTpXZw",
+            "title":"Do you like pizza?",
+            "type":"yes_no",
+            "ref":"boolean",
+            "properties":{
+
+            }
+         },
+         {
+            "id":"IVzkMhrHh9a8",
+            "title":"What do you put on your pizza?",
+            "type":"multiple_choice",
+            "allow_multiple_selections":true,
+            "allow_other_choice":true,
+            "ref":"multiple_choice",
+            "properties":{
+
+            },
+            "choices":[
+               {
+                  "id":"ig0PVrSFiUS5",
+                  "label":"Nothing, it's already perfect"
+               },
+               {
+                  "id":"WknO1AuKpXlN",
+                  "label":"Pepperoni"
+               },
+               {
+                  "id":"XQatQTEwoj5b",
+                  "label":"Oregano"
+               },
+               {
+                  "id":"r1LNBNZWMp2T",
+                  "label":"Eggplant"
+               },
+               {
+                  "id":"cW5U58DrBRft",
+                  "label":"Olives"
+               }
+            ]
+         },
+         {
+            "id":"HkBESHByy7LI",
+            "title":"What do you drink while eating pizza?",
+            "type":"short_text",
+            "ref":"short_text",
+            "properties":{
+
+            }
+         },
+         {
+            "id":"EtTRuQkE4hjk",
+            "title":"What do you think about people who don't like pizza?",
+            "type":"long_text",
+            "ref":"long_text",
+            "properties":{
+
+            }
+         },
+         {
+            "id":"xTwhj9PkF3Hz",
+            "title":"What of these pizza slices you would have?",
+            "type":"picture_choice",
+            "ref":"picture_choice",
+            "properties":{
+
+            },
+            "choices":[
+               {
+                  "id":"OLUgCN5bpAtk",
+                  "label":"Pepperoni"
+               },
+               {
+                  "id":"NzGAfQhbAVNO",
+                  "label":"Mediterranean"
+               },
+               {
+                  "id":"mdW5VVa6iQzA",
+                  "label":"Ham and cheddar"
+               }
+            ]
+         },
+         {
+            "id":"MWnRmsfmoByz",
+            "title":"How much would you like pizza right now?",
+            "type":"opinion_scale",
+            "ref":"option_scale",
+            "properties":{
+
+            }
+         },
+         {
+            "id":"EswVlznrQiX5",
+            "title":"Write your email to receive news about pizza?",
+            "type":"email",
+            "ref":"email",
+            "properties":{
+
+            }
+         },
+         {
+            "id":"elYYECCcCzkc",
+            "title":"When's the last time you had pizza?",
+            "type":"date",
+            "ref":"data",
+            "properties":{
+
+            }
+         },
+         {
+            "id":"Lzb4em2zDvSu",
+            "title":"How many slices did you have?",
+            "type":"number",
+            "ref":"number",
+            "properties":{
+
+            }
+         },
+         {
+            "id":"nGfcKUzurcej",
+            "title":"Where do you want to have your next pizza?",
+            "type":"dropdown",
+            "ref":"dropdown",
+            "properties":{
+
+            }
+         },
+         {
+            "id":"s9DPZ6OBZNkr",
+            "title":"If you accept this term you declare pizza as the best meal ever created by the human species.",
+            "type":"legal",
+            "ref":"legal",
+            "properties":{
+
+            }
+         },
+         {
+            "id":"gvK23LCKLZM6",
+            "title":"Upload a photo you took of a pizza you had.",
+            "type":"file_upload",
+            "ref":"file",
+            "properties":{
+
+            }
+         },
+         {
+            "id":"yLV97kWxKB7i",
+            "title":"What do you think about having pizza for breakfast?",
+            "type":"rating",
+            "ref":"rating",
+            "properties":{
+
+            }
+         },
+         {
+            "id":"tr7WgzVrvqnd",
+            "title":"Share your favorite website to look for pizza places...",
+            "type":"website",
+            "ref":"url",
+            "properties":{
+
+            }
+         },
+         {
+            "id":"it8UEW31ZlCb",
+            "title":"Do you have friends that doesn't like pizza?",
+            "type":"yes_no",
+            "ref":"grouped_boolean",
+            "properties":{
+
+            }
+         },
+         {
+            "id":"JjpjdSGicVx1",
+            "title":"If so, share their names...",
+            "type":"short_text",
+            "ref":"grouped_short_text",
+            "properties":{
+
+            }
+         }
+      ]
+   },
+   "answers":[
+      {
+         "type":"boolean",
+         "boolean":true,
+         "field":{
+            "id":"NGXQV4HTpXZw",
+            "type":"yes_no",
+            "ref":"22babd80-a80c-47c1-9213-9bc0161f3668"
+         }
+      },
+      {
+         "type":"choices",
+         "choices":{
+            "labels":[
+               "Oregano"
+            ]
+         },
+         "field":{
+            "id":"IVzkMhrHh9a8",
+            "type":"multiple_choice",
+            "ref":"e6a16aa7-1109-4690-8af4-e6d45cfa49f4"
+         }
+      },
+      {
+         "type":"text",
+         "text":"Beer",
+         "field":{
+            "id":"HkBESHByy7LI",
+            "type":"short_text",
+            "ref":"4fe9a8f8-a0c8-489f-aab3-d421a4d96d43"
+         }
+      },
+      {
+         "type":"text",
+         "text":"They should stop eating and die",
+         "field":{
+            "id":"EtTRuQkE4hjk",
+            "type":"long_text",
+            "ref":"5a5d5c15-d7bb-4ad5-b3fb-d1f4d7aab746"
+         }
+      },
+      {
+         "type":"choice",
+         "choice":{
+            "label":"Mediterranean"
+         },
+         "field":{
+            "id":"xTwhj9PkF3Hz",
+            "type":"picture_choice",
+            "ref":"e9032af2-0b22-46dc-b389-071a1674141b"
+         }
+      },
+      {
+         "type":"number",
+         "number":5,
+         "field":{
+            "id":"MWnRmsfmoByz",
+            "type":"opinion_scale",
+            "ref":"56903033-de28-4abb-99b3-7cc0b0c82532"
+         }
+      },
+      {
+         "type":"email",
+         "email":"me@pizza.net",
+         "field":{
+            "id":"EswVlznrQiX5",
+            "type":"email",
+            "ref":"6f30b8a0-9da9-4623-9715-03d22ed394cb"
+         }
+      },
+      {
+         "type":"date",
+         "date":"2018-10-21",
+         "field":{
+            "id":"elYYECCcCzkc",
+            "type":"date",
+            "ref":"52d4d570-a842-4870-b75e-fc44a32fd72a"
+         }
+      },
+      {
+         "type":"number",
+         "number":6,
+         "field":{
+            "id":"Lzb4em2zDvSu",
+            "type":"number",
+            "ref":"bed56386-7d0d-4ef6-959d-1e12fb52ff1b"
+         }
+      },
+      {
+         "type":"choice",
+         "choice":{
+            "label":"Italy"
+         },
+         "field":{
+            "id":"nGfcKUzurcej",
+            "type":"dropdown",
+            "ref":"6522c699-ee46-4db6-829c-c619219871e0"
+         }
+      },
+      {
+         "type":"boolean",
+         "boolean":true,
+         "field":{
+            "id":"s9DPZ6OBZNkr",
+            "type":"legal",
+            "ref":"572da572-0a1d-4d68-ae13-f5ba97d259ad"
+         }
+      },
+      {
+         "type":"file_url",
+         "file_url":"https://admin.typeform.com/form/results/file/download/CEIcOU/gvK23LCKLZM6/e35015251422-41304832_283393435601513_2743585903397131013_n.jpg?key=aHR0cHM6Ly90eXBlZm9ybS1maWxlLXVwbG9hZC5zMy5hbWF6b25hd3MuY29tL0NFSWNPVS9ndksyM0xDS0xaTTYvZTM1MDE1MjUxNDIyLTQxMzA0ODMyXzI4MzM5MzQzNTYwMTUxM18yNzQzNTg1OTAzMzk3MTMxMDEzX24uanBnP1gtQW16LUFsZ29yaXRobT1BV1M0LUhNQUMtU0hBMjU2JlgtQW16LUNyZWRlbnRpYWw9QUtJQUpYWVRRQzVURElTQlQ1M1ElMkYyMDE4MTAyNiUyRnVzLWVhc3QtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAxODEwMjZUMTAzNjA2WiZYLUFtei1FeHBpcmVzPTg2NDAwJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZYLUFtei1TaWduYXR1cmU9NWQ0NDE2NmNmMGM0M2YwZjgzNTgwNjFiNTY4OTI5NjNlMGE3ZmZjYTI3YTVlYmY1ZWJmNGVlOTIzZmEwYTk2NQ%3D%3D",
+         "field":{
+            "id":"gvK23LCKLZM6",
+            "type":"file_upload",
+            "ref":"2de3d1e5-bca0-463a-baa3-ffba3e6fe135"
+         }
+      },
+      {
+         "type":"number",
+         "number":5,
+         "field":{
+            "id":"yLV97kWxKB7i",
+            "type":"rating",
+            "ref":"1a8a1def-64a9-4fab-aecf-455d49e513d9"
+         }
+      },
+      {
+         "type":"url",
+         "url":"https://www.google.com/maps/search/pizza",
+         "field":{
+            "id":"tr7WgzVrvqnd",
+            "type":"website",
+            "ref":"da3deac1-94b4-4461-9f55-101429ec6009"
+         }
+      },
+      {
+         "type":"boolean",
+         "boolean":true,
+         "field":{
+            "id":"it8UEW31ZlCb",
+            "type":"yes_no",
+            "ref":"43fb28f0-739d-4186-91a3-a9b9aeffd4d0"
+         }
+      },
+      {
+         "type":"text",
+         "text":"Phoebe Buffay",
+         "field":{
+            "id":"JjpjdSGicVx1",
+            "type":"short_text",
+            "ref":"3cd0b2dd-73a5-4a08-8ad4-d0eeb00699c1"
+         }
+      }
+   ]
+}
