67a801b9d9594500cd17ef341003b16605aa51c3
Jere Menichelli <jmenichelli@gmail.com>
2018-12-08 11:16:58 +0100

    fix: add support for refs in answers fields (Response API)

--- test/get-answer-value-from-ref.test.js
+++ test/get-answer-value-from-ref.test.js
@@ -21,3 +21,22 @@ test('returns undefined with not found ref', t => {
   const result = helpers.getAnswerValueFromRef('does_not_exist', payload)
   t.is(result, undefined);
 });
+
+test('returns correct answer for response API payloads', t => {
+  const ref = "4fe9a8f8-a0c8-489f-aab3-d421a4d96d43"
+  const __mocked__ = {
+    answers: [ 
+      {
+        "type": "text",
+        "text": "Beer",
+        "field": {
+          "id": "HkBESHByy7LI",
+          "type": "short_text",
+          ref
+        }
+      }
+    ]
+  }
+  const result = helpers.getAnswerValueFromRef(ref, __mocked__)
+  t.is(result, 'Beer');
+});
